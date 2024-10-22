092878d5a000272b0987350b3846e045b40ccb21
Anthony Nova <anthony.nova@tecsup.edu.pe>
2024-10-22 16:12:44 -0500

    cambios finales

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 //listas
-//en esta seccion van las urls de las imagenes con , justo despues de cada url
-const urls = 'https://scontent.faqp3-1.fna.fbcdn.net/v/t39.30808-6/462787172_1127566562275453_8540253778161777727_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeH4GkMDtyZCvhJlDbolyQIQXdAvWqgtl1Nd0C9aqC2XU7xKN-aBR2E-kkSX576H0ZMLQf4y_YyLU5Tp_gQyFrmt&_nc_ohc=HJSZPJf7YaUQ7kNvgHXGoan&_nc_ht=scontent.faqp3-1.fna&_nc_gid=Ax_-tkhzLcv_H71dG9PFfLk&oh=00_AYCwz47zGVnTVloDzX8Mp1LGRHj2XOS2-Hc_JctWA4OeLg&oe=671B052E,https://scontent.faqp3-1.fna.fbcdn.net/v/t39.30808-6/462711287_1128225752209534_4836917110832899732_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEu-k2iHxb7krvU2y9feA1JzdU7HnJ3GfHN1TsecncZ8atIKXEQg84Kib9cIUGe77kD9aZOk9jZWQ7rwE3J4Shr&_nc_ohc=QMW6vb5BYWkQ7kNvgFTErJ8&_nc_ht=scontent.faqp3-1.fna&_nc_gid=Ar5kuN-9mcX2E7xasdKnJAv&oh=00_AYCtpn9s8fuj1hQFn_3Wadg0o-LY94qUXmAkOpK7UuqbMQ&oe=671B0922,https://scontent.faqp3-1.fna.fbcdn.net/v/t39.30808-1/454908537_1083298923368884_5031723277163585488_n.jpg?stp=c137.0.693.693a_dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=f4b9fd&_nc_eui2=AeF_1mJ5utXBsgckYJmzi0VVf5AsNNdzK-x_kCw013Mr7PE5EfpGBBtAC2kviGGBLZpFpFHOfCts-grr-o-BdTH6&_nc_ohc=QfAzN3tDjp0Q7kNvgEZ_os8&_nc_ht=scontent.faqp3-1.fna&_nc_gid=Ai6_5UxwKGJjKnr8tnwlzo3&oh=00_AYAl_egCWo5cBkxuNjTVxiulPdIYgOTlwSrCwL1BuVtMTg&oe=671AE55A,https://scontent.faqp3-1.fna.fbcdn.net/v/t39.30808-6/321785108_6540848099263568_2178004762697756724_n.jpg?stp=dst-jpg_s960x960&_nc_cat=109&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHETySCB-ZRd4cCk_uUoUvxc0AUzzYaFVVzQBTPNhoVVVgia5-Mj57DsDRzzYCJEXIqn7MrD4mNAVHBCu-TtFw4&_nc_ohc=XvwKRjlehkoQ7kNvgHjTqPy&_nc_ht=scontent.faqp3-1.fna&_nc_gid=Ai6_5UxwKGJjKnr8tnwlzo3&oh=00_AYAXE4zShqu5mZXLM2pFM14BCrNibzzvNqUjHKZIvRQnuA&oe=671B079C';
+//en esta seccion van las urls de las imagenes con "," justo despues de cada url
+const urls = 'https://lh3.googleusercontent.com/pw/AP1GczNcg14QTYnLz67woS0moDn76ZJyQRRSsu2sd6Uwb_Ru73ohEv8mMZYlIsjR1lHdlH5vLCl6ANgBawDNAG8ldqBZcyy45Lo7tITwCAmtd_q52BzVR6o=w2400,https://scontent.faqp3-1.fna.fbcdn.net/v/t39.30808-6/462787172_1127566562275453_8540253778161777727_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeH4GkMDtyZCvhJlDbolyQIQXdAvWqgtl1Nd0C9aqC2XU7xKN-aBR2E-kkSX576H0ZMLQf4y_YyLU5Tp_gQyFrmt&_nc_ohc=HJSZPJf7YaUQ7kNvgHXGoan&_nc_ht=scontent.faqp3-1.fna&_nc_gid=Ax_-tkhzLcv_H71dG9PFfLk&oh=00_AYCwz47zGVnTVloDzX8Mp1LGRHj2XOS2-Hc_JctWA4OeLg&oe=671B052E,https://scontent.faqp3-1.fna.fbcdn.net/v/t39.30808-6/462711287_1128225752209534_4836917110832899732_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEu-k2iHxb7krvU2y9feA1JzdU7HnJ3GfHN1TsecncZ8atIKXEQg84Kib9cIUGe77kD9aZOk9jZWQ7rwE3J4Shr&_nc_ohc=QMW6vb5BYWkQ7kNvgFTErJ8&_nc_ht=scontent.faqp3-1.fna&_nc_gid=Ar5kuN-9mcX2E7xasdKnJAv&oh=00_AYCtpn9s8fuj1hQFn_3Wadg0o-LY94qUXmAkOpK7UuqbMQ&oe=671B0922,https://scontent.faqp3-1.fna.fbcdn.net/v/t39.30808-1/454908537_1083298923368884_5031723277163585488_n.jpg?stp=c137.0.693.693a_dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=f4b9fd&_nc_eui2=AeF_1mJ5utXBsgckYJmzi0VVf5AsNNdzK-x_kCw013Mr7PE5EfpGBBtAC2kviGGBLZpFpFHOfCts-grr-o-BdTH6&_nc_ohc=QfAzN3tDjp0Q7kNvgEZ_os8&_nc_ht=scontent.faqp3-1.fna&_nc_gid=Ai6_5UxwKGJjKnr8tnwlzo3&oh=00_AYAl_egCWo5cBkxuNjTVxiulPdIYgOTlwSrCwL1BuVtMTg&oe=671AE55A';
 //aqui van los trofeos
 const trofeos = [
-  { nombre: "Trofeo del mejor cliente", descripcion: "Premio al mejor rendimiento", emoji: "🥇" },
-  { nombre: "Trofeo de Plata", descripcion: "Premio al segundo lugar", emoji: "🥈" },
-  { nombre: "Trofeo de Bronce", descripcion: "Premio al tercer lugar", emoji: "🥉" },
-  { nombre: "Trofeo de Participación", descripcion: "Premio a la participación", emoji: "🏆" },
-  { nombre: "Trofeo de Esfuerzo", descripcion: "Premio al esfuerzo", emoji: "🏆" },
-  { nombre: "Trofeo de Esfuerzo", descripcion: "Premio al esfuerzo", emoji: "🏆" },
-  { nombre: "Trofeo de Participación", descripcion: "Premio a la participación", emoji: "🏆" },
-  { nombre: "Trofeo de Participación", descripcion: "Premio a la participación", emoji: "🏆" },
+  { nombre: "Trofeo del mejor cliente", descripcion: "Premio al mejor rendimiento", emoji: "" },
+  { nombre: "Trofeo de Plata", descripcion: "Premio al segundo lugar", emoji: "" },
+  { nombre: "Trofeo de Bronce", descripcion: "Premio al tercer lugar", emoji: "" },
+  { nombre: "Trofeo de Participación", descripcion: "Premio a la participación", emoji: "" },
+  { nombre: "Trofeo de Esfuerzo", descripcion: "Premio al esfuerzo", emoji: "" },
+  { nombre: "Trofeo de Esfuerzo", descripcion: "Premio al esfuerzo", emoji: "" },
+  { nombre: "Trofeo de Participación", descripcion: "Premio a la participación", emoji: "" },
+  { nombre: "Trofeo de Participación", descripcion: "Premio a la participación", emoji: "" },
 ];
-const backgroundUrl = '../fondo.png'; // Aquí va el link de la imagen de fondo
-const youtubeVideoId = 'Y0ywrp-EBXY'; // Aquí defines solo el ID del video
+//ulr del fondo:
+const backgroundUrl = 'https://wallpapers.com/images/featured/1920x1080-aesthetic-glrfk0ntspz3tvxg.webp'; 
+// Aquí defines solo el ID del video
+const youtubeVideoId = 'Cunvu8b7rj0';
 
+
+
+
+
+//Todo codigo
 const urlsArray = urls.trim().split(',');
 const imagenes = urlsArray.map((url, index) => ({
   src: url.trim(),
